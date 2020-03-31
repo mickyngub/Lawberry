12adbe898bcc88cce7dde668fe8ea087c1f6286a
climaxhome <57805640+climaxhome@users.noreply.github.com>
2020-03-31 09:49:42 +0700

    Create New Page

--- src/App.tsx
+++ src/App.tsx
@@ -5,13 +5,22 @@ import Layout from "./components/Layout/Layout";
 // import classes from "./App.module.css";
 import Aux from "./hoc/Aux";
 // import ExternalLinks from "./containers/ExternalLinks/ExternalLinks";
+import { Route, Switch } from "react-router-dom";
+import HomePage from "./containers/HomePage/HomePage";
 
 function App() {
   return (
     // <div className="App">
     <Aux>
       {/* <Router> */}
-      <Layout></Layout>
+      <Layout>
+        <Switch>
+          <Route path="/externalLinks" component={HomePage} />
+          <Route exact path="/">
+            <HomePage />
+          </Route>
+        </Switch>
+      </Layout>
       {/* </Router> */}
     </Aux>
     // </div>
